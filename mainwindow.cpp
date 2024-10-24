df992d3c2227c2e63691225f90aa629fb439ecc4
RFN_KIRAME <13787701007@sina.cn>
2024-10-24 18:34:04 +0800

    1.将ElaAppBarPrivate.cpp中第54行判断注释以规避异常 2.增加测试控件

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 
 #include "ElaNavigationBar.h"
+#include <QString>
 
 mainwindow::mainwindow(ElaWindow* parent)
     : ElaWindow(parent)
@@ -11,11 +12,17 @@ mainwindow::mainwindow(ElaWindow* parent)
     setProperty("ElaBaseClassName", "ElaWindow");
     resize(1020, 680); // д╛хо©М╦ъ
 
-    this->setIsNavigationBarEnable(false);
+    /*this->setIsNavigationBarEnable(false);
 
     QWidget* centralWidget = new QWidget(this);
     this->setCentralWidget(centralWidget);
-    centralWidget->installEventFilter(this);
+    centralWidget->installEventFilter(this);*/
+
+    QWidget* wg_t1 = new QWidget(this);
+    QWidget* wg_t2 = new QWidget(this);
+
+    ui->naviBar->addPageNode(QString("Test1"), wg_t1, ElaIconType::AlarmClock);
+    ui->naviBar->addPageNode(QString("Test2"), wg_t2, ElaIconType::Airplay);
 }
 
 mainwindow::~mainwindow()
